22f02f1c7854fa3aa329b1c50d6a857a9accee78
RaimondasTvaska <rtvaska@gmail.com>
2021-06-05 13:06:38 +0300

    Second commit

--- index.php
+++ index.php
@@ -7,38 +7,57 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+<style>
+        table {
+          font-family: arial, sans-serif;
+          border-collapse: collapse;
+          width: 100%;
+        }
+        
+        td, th {
+          border: 1px solid #dddddd;
+          text-align: left;
+          padding: 8px;
+        }
+        
+        tr:nth-child(even) {
+          background-color: #dddddd;
+        }
+</style>
 
 
 <body>
 <?php
 session_start();
 echo "<script>";
-    if(isset($_SESSION['result'])){
-    
-           echo" const data = ".$_SESSION['result'];
-       
-    }else{
-        echo" const data = 0";
-    }
-    echo  "</script>";
-
+if (isset($_SESSION['result'])) {
+    echo " const data = " . $_SESSION['result'];
+} else {
+    echo " const data = 0";
+}
+if(isset( $_SESSION['query'])){
+    $q = $_SESSION['query'];
+}else{
+    $q = "";
+}
+echo "</script>";
 ?>
     <form action="./link.php" method="get">
-        <input name="query" type="hidden"  value="SELECT * from `sales` where 1">
+        <input name="query" type="hidden"  value="SELECT * from  `sales` where 1">
         <button type="submit">uzkrauti duomenis</button>
     </form>
     <br>
     <br>
     <form action="./link.php" method="get">
-    <textarea id="w3review" name="query" rows="4" cols="50"  ></textarea>
+    <textarea id="w3review" name="query" rows="10" cols="50"  ><?php echo $q ?></textarea>
         <button type="submit">siusti uzklausa</button>
     </form>
     
+<table id="table">
 
+</table>
 
-    <script>
-        console.log(data);
-    </script>
+    <script src="./render.js"></script>
 </body>
 
 </html>
---
+++ render.js
@@ -0,0 +1,24 @@
+console.log(data);
+
+
+    
+
+let table = document.getElementById('table');
+let HTML = "";
+HTML += "<tr>";
+
+Object.keys(data[0]).forEach(key =>{
+    HTML += `<th>${key}</th>`;
+})
+HTML += "</tr>";
+
+data.forEach(item => {
+    HTML += "<tr>";
+    Object.values(item).forEach(value =>{
+        HTML += `<td>${value}</td>`;
+    });
+    HTML += "</tr>";
+})
+table.innerHTML += HTML;
+
+
